e416c8487e96c77aa02a0eaab6b27baeebd26320
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-10-13 19:34:45 -0400

    like function works!

--- src/backend/Upload.js
+++ src/backend/Upload.js
@@ -1,5 +1,8 @@
 import fs from 'fs';
 import { unminify } from './unMinify.js';
+import { error } from 'console';
+const POSTPATH = './postsMin.json';
+
 class post{
     constructor(userId, bodyText, hashTags, videoId = null, imageId = null, time = performance.now()){
         this.userId = userId;
@@ -15,9 +18,6 @@ class post{
     }
 
     // the following functions need to be able to update the existing post.json value
-    like(){
-        this.like ++;
-    }
     view(){
         this.views ++;
     }
@@ -25,8 +25,22 @@ class post{
         this.reports ++;
     }
 }
+function like(postId) {
+    const postsJsonObject = JSON.parse(fs.readFileSync(POSTPATH, 'utf8'));
+    const postObject = postsJsonObject.find(post => post.postId === postId);
 
-// needs to check if the id doesnt exist and create a new one
+    if (!postObject) {
+        console.error('Post not found!');
+        return;
+    }
+    console.log(postObject);
+    //saves to a new object, not the old json
+    postObject.like++;
+    postsJsonObject[postObject.postId] = postObject;
+
+    fs.writeFileSync(POSTPATH, JSON.stringify(postsJsonObject, null, 2), 'utf8');
+}
+
 function getUniqueId() {
     const postsJsonObject = JSON.parse(fs.readFileSync('postsMin.json', 'utf8'));
 
@@ -42,21 +56,16 @@ function getUniqueId() {
         uniqueId = Math.floor(Math.random() * 100000);
     } while (existingPostIds.has(uniqueId));
 
-    // Return the unique ID.
     return uniqueId;
 }
-
-
 function saveNewPost(unqiuePost){
-    const jsonFilepath = './postsMin.json';
-    const jsonFileContents = fs.readFileSync(jsonFilepath, 'utf8');
+    const jsonFileContents = fs.readFileSync(POSTPATH, 'utf8');
 
     const jsonObject = JSON.parse(jsonFileContents);
     jsonObject.push(unqiuePost);
-    const jsonString = JSON.stringify(jsonObject);
+    const jsonString = JSON.stringify(jsonObject, null, 2);
 
-    fs.writeFileSync(jsonFilepath, jsonString, 'utf8');
-    unminify(jsonFilepath);
+    fs.writeFileSync(POSTPATH, jsonString, 'utf8');
 }
-
-saveNewPost(new post('1', "Fahad's first post", ['firstpost', '1', '2']));
+// saveNewPost(new post('1', "Fahad's first post", ['firstpost', '1', '2']));
+// like(2);
