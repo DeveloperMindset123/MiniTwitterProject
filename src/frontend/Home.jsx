1c3c155d3d0bfef27e55ff8034581f7097152276
Hamzah Bemat <hamzahbemat23@gmail.com>
2023-10-18 13:10:59 -0400

    Home

--- src/frontend/Home.jsx
+++ src/frontend/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './style.css'; 
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div className="home-container">
+      <div className="content">
+        <h1>Home</h1>
+      </div>
+      <div className="button-container">
+        <button className="main-button">Main</button>
+        <button className="trending-button">Trending</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
