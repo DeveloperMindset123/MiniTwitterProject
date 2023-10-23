c83a7ca80644154d61ba1bb636c60236ceeff45b
Hamzah Bemat <hamzahbemat23@gmail.com>
2023-10-23 15:18:09 -0400

    NavBar

--- src/frontend/Home.jsx
+++ src/frontend/Home.jsx
@@ -4,6 +4,18 @@ import './style.css';
 const Home = () => {
   return (
     <div className="home-container">
+      <div className="sidebar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Notifications</a></li>
+          <li><a href="#">Community</a></li>
+          <li><a href="#">Bookmarks</a></li>
+          <li><a href="#">Profile</a></li>
+          <li><a href="#">Settings</a></li>
+          <li><a href="#">Lists</a></li>
+          <li><a href="#">More</a></li>
+        </ul>
+      </div>
       <div className="content">
         <h1>Home</h1>
       </div>
