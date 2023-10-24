2b015716e3de060159965b5cf726b864c4b6bd38
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-10-24 17:15:49 -0400

    added mongodb and express server

---
+++ client/vite.config.cjs
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+const express = require('express');
+const app = express();
+const port = 3001;
+
+// Add your Express server logic here
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // base: '/MiniTwitterProject/',
+})
