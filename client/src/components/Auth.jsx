caf6b4b4e0953771010d6fa0d1a7be206200daac
sxl7 <lshuxin4@gmail.com>
2023-11-01 23:02:29 -0400

    Merge branch 'main' of https://github.com/DeveloperMindset123/MiniTwitterProject

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,18 +4,48 @@ import React, { useState } from 'react';
 import '../styles/Auth.css';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-
+import Form from 'react-bootstrap/Form';
 //import Selection from './Selection'
 
 export default function Auth(props) {
 
     let [authMode, setAuthMode] = useState("signin");
+    const [isCorporateUser, setIsCorporateUser] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isSelectionModalOpen, setIsSelectionModalOpen] = useState(false);
 
 
     const changeAuthMode = () => { //set the logic for whether to sign in or register
         setAuthMode(authMode == "signin" ? "signup" : "signin")
     }
 
+    const handleCorporateUserChange = () => {
+    	setIsCorporateUser(!isCorporateUser);
+	};
+    const openCorporateModal = () => {
+        setIsModalOpen(true);
+    };
+    const closeCorporateModal = () => {
+        setIsModalOpen(false);
+    };
+    const openSelectionModal = () => {
+        setIsSelectionModalOpen(true);
+    };
+
+    const closeSelectionModal = () => {
+        setIsSelectionModalOpen(false);
+    };
+	const handleSubmit = (event) => {
+        event.preventDefault(); 
+        if (isCorporateUser) {
+            openCorporateModal();
+        } else{
+            openSelectionModal();
+        }
+    };
+   
+
+ 
     if (authMode == 'signin') {
         return (
             <div className="Auth-form-container">
@@ -44,6 +74,7 @@ export default function Auth(props) {
                                 placeholder='Enter Password'
                             />
                         </div>
+                        
                         <div className='d-grid gap-2 mt-3'>
                             <button type='Submit' className='btn btn-primary'>
                                 Submit
@@ -105,25 +136,209 @@ export default function Auth(props) {
                             placeholder='Retype Password'
                         />
                     </div>
-                    <div>
-                         <Selection />
+                    <div className="form-group mt-3">
+                            <input
+                                type="checkbox"
+                                className="form-check-input"
+                                checked={isCorporateUser}
+                                onChange={handleCorporateUserChange}
+                                style={{ marginRight: "10px" }}
+                            />
+                            <label>Corporate User</label>
+                        </div>
+                        
+                        <div className='d-grid gap-2 mt-3'>
+                        <button type='Submit' className='btn btn-primary' onClick={handleSubmit}>
+                            Submit
+                        </button>
                     </div>
                     <p className='text-center mt-2'>
                         Forgot <a href='#'>Password?</a>
                     </p>
                 </div>
             </form>
+            {isCorporateUser && (
+                <Corporate isModalOpen={isModalOpen} closePopup={closeCorporateModal} />
+            )}
+            {isSelectionModalOpen && <Selection closeSelectionModal={closeSelectionModal} />}
+
         </div>
 
     )
 }
+function Corporate({ isModalOpen, closePopup }) {
+    const [isCustomerTargetModalOpen, setIsCustomerTargetModalOpen] = useState(false);
+
+ 
+    const openCustomerTargetModal = () => {
+        setIsCustomerTargetModalOpen(true);
+    };
+
+    const closeCustomerTargetModal = () => {
+        setIsCustomerTargetModalOpen(false);
+    };
+    
+    return (
+    <div>
+        <Modal show={isModalOpen} onHide={closePopup} className="CorporateModal">
+            <Modal.Header closeButton>
+                <Modal.Title>Corporate Information</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="corporateInformation">
+                    <div className="corporate">
+                        <label>Company Name</label>
+                        <input type="text" placeholder="Enter your company name" />
+                    </div>
+                    <div className="corporate">
+                        <label>Phone Number</label>
+                        <input type="text" placeholder="Enter your phone number" />
+                    </div>
+                    <div className="corporate">
+                        <label>Address</label>
+                        <input type="address" placeholder="Enter your address" />
+                    </div>
+                    <div className="corporate">
+                        <label>City</label>
+                        <input type="address" placeholder="Enter your city" />
+                    </div>
+                    <div className="corporate">
+                        <label>State</label>
+                        <input type="address" placeholder="Enter your state" />
+                    </div>
+                    <div className="corporate">
+                        <label>Zip Code</label>
+                        <input type="address" placeholder="Enter the Zip Code" />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button
+                            className="custom-button"
+                            onClick={openCustomerTargetModal} 
+                        >
+                        Continue
+                        </button>
+                </div>
+                </div>
+            </Modal.Body>
+        </Modal>
+
+        {isCustomerTargetModalOpen && (
+                <CustomerTarget
+                    isCustomerTargetModalOpen={isCustomerTargetModalOpen}
+                    closeCustomerTargetModal={closeCustomerTargetModal}
+                />
+            )}
+        </div>
+
+    );
+}
+
+function CustomerTarget({isCustomerTargetModalOpen, closeCustomerTargetModal }) {
+   
+    return (
+        <Modal show={isCustomerTargetModalOpen} onHide={closeCustomerTargetModal} className="custom-modal">
+            <Modal.Header closeButton>
+                <Modal.Title>Customer Target</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                        <Form.Group>
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Influencer"
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Gamer"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Business Professional"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Parent"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Student"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Tech Enthusiast"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Fitness Enthusiast"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Pet Owner"
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Homeowner"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Traveler"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Fashionistas"
+                                
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Foodie"
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Outdoor Adventurer"
+                            />
+                            <Form.Check
+                                className="custom-checkbox"
+                                type="checkbox"
+                                label="Shopper"
+                            />
+                        </Form.Group>
+                    </Form>
+                    <div className="d-grid gap-2 mt-3">
+                    <Button className="custom-button"variant="primary">
+                        Next
+                    </Button>
+                </div>
+            </Modal.Body>
+        </Modal>
+    );
+}
 
 function Selection() {
     const [show, setShow] = useState(false);
-  
+
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-
 
     const [selectedInterests, setSelectedInterests] = useState([]);
     
@@ -153,19 +368,20 @@ function Selection() {
         }
       };
 
-    const handleSubmit = (e) => {
+      const handleSubmit = (e) => {
         e.preventDefault()
-        handleShow();
-  };
+            handleShow();
+      };
+
 
   
     return (
       <>
-          <div className='d-grid gap-2 mt-3'>
-              <button type='Submit' className='btn btn-primary' onClick={handleSubmit}>
-                  Submit
-              </button>
-          </div>
+        <div className='d-grid gap-2 mt-3'>
+                        <button type='Submit' className='btn btn-primary' onClick={handleSubmit}>
+                            Submit
+                        </button>
+                    </div>
   
         <Modal
           show={show}
